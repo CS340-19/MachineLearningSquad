876af704843b92c54aacb873d70d6b8d52e2038b
Kody Bloodworth <kbloodwo@vols.utk.edu>
2019-03-29 02:20:59 -0400

    Added tkinter widget to help with demo

--- AutoStories/popup/choose_page.js
+++ AutoStories/popup/choose_page.js
@@ -1,21 +1,26 @@
 function GetTweetStories(){
 	if(window.location.href.match(/twitter.com/g) != null){
 		var path = window.location.pathname;
-		var 
-		
+		var n = path.lastIndexOf("/");
+		var id = path.substr(n+1);
+		console.log(id);	
+	}else{
+		document.getElementById("story-1").innerHTML = "Not a Valild Page! Sorry!";
 	}
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
-		if(this.readyState == 4 && thiis.status == 200){
-			var stories = this.responseText;
-			var objs = JSON.parse(stories);
-			document.getElementById("story-1").innerHTML = objs[0].title;
+		if(this.readyState == 4 && this.status == 200){
+			//var stories = this.responseText;
+			//var objs = JSON.parse(stories);
+			//console.log(objs[0].title);
+			document.getElementById("story-1").innerHTML = "hey";//objs[0].title;
 		}else{
+			console.log("failed server");
 			document.getElementById("story-1").innerHTML = "Not a Valid Page! Sorry!";
 		}
 	};
-	xhttp.open("GET", 
-
+	xhttp.open("GET", "url.txt", true); 
+	xhttp.send();
 }
 
 document.addEventListener("click", function (e) {
---
+++ TweetStoryGUI.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+import requests
+import json
+
+class Window(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent, width=150, height=50)
+        self.boxes = list()
+        self.submit = tk.Button(self, text = "Get Stories", command=self.GetStoriesFromUrl)
+        self.submit.pack()
+        self.url = tk.Entry(self, width = 75)
+        self.url.pack()
+    def GetStoriesFromUrl(self):
+        for x in self.boxes:
+            x.pack_forget();
+        s = self.url.get()
+        s = s[s.rfind("/")+1:]
+        url = "http://107.161.31.193:5000/?tweetid="+s
+        print(url)
+        r = requests.get(url)
+        if r.status_code != 200:
+            print("bad url");
+            quit()
+        stories = r.json();
+        for x in stories:
+            print(x)
+            v = tk.Label(self, text = x["title"], font=("Helvetica", 16))
+            self.boxes.append(v)
+            v.pack();
+
+            d = tk.Label(self, text = x["description"], font=("Helvetica", 12))
+            self.boxes.append(d)
+            d.pack()
+
+
+root = tk.Tk()
+W = Window(root)
+W.pack()
+root.mainloop()
