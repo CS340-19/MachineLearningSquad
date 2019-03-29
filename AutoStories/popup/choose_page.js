75af6d49ee1149fe6efe2f32e346fbbbcd8716de
Kody Bloodworth <kbloodwo@vols.utk.edu>
2019-03-28 23:58:03 -0400

    Getting Tweet ID from URL

--- AutoStories/popup/choose_page.js
+++ AutoStories/popup/choose_page.js
@@ -1,3 +1,23 @@
+function GetTweetStories(){
+	if(window.location.href.match(/twitter.com/g) != null){
+		var path = window.location.pathname;
+		var 
+		
+	}
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if(this.readyState == 4 && thiis.status == 200){
+			var stories = this.responseText;
+			var objs = JSON.parse(stories);
+			document.getElementById("story-1").innerHTML = objs[0].title;
+		}else{
+			document.getElementById("story-1").innerHTML = "Not a Valid Page! Sorry!";
+		}
+	};
+	xhttp.open("GET", 
+
+}
+
 document.addEventListener("click", function (e) {
     if (!e.target.classList.contains("page-choice")) {
         return;
